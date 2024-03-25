a7c2d077f1e28431b73e32a3cc570056223ebd5b
mrdavtan <mrdavtan@protonmail.com>
2024-03-24 20:45:03 -0700

    fixed auto loading of graph after input or voice text

--- server.js
+++ server.js
@@ -30,31 +30,6 @@ app.get('/api/graphs', async (req, res) => {
 });
 
 
-app.post('/api/start-bot2', (req, res) => {
-  const bot2Process = spawn('python', ['bot2.py']);
-  bot2Process.on('close', (code) => {
-    console.log(`bot2.py process exited with code ${code}`);
-  });
-  res.sendStatus(200);
-});
-
-app.get('/api/latest-graph', (req, res) => {
-  fs.readdir('public/graphs', (err, files) => {
-    if (err) {
-      console.error('Error reading graphs directory:', err);
-      res.sendStatus(500);
-    } else {
-      if (files.length === 0) {
-        res.sendStatus(404); // Send 404 if no graph files found
-      } else {
-        const latestFile = files.sort().pop();
-        res.json({ latestFile });
-      }
-    }
-  });
-});
-
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
